bd34ace448e1a4f866ed6e969b8e061c15ca3bad
Tina <liutianli2011@gmail.com>
2020-11-19 09:14:49 +0800

    implemented home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,41 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export class Home extends Component {
-   
+    
+    state = {
+        patientData: []
+    }
+
+    componentDidMount(){
+        axios.get("http://localhost:9090/api/patients").then(res=>{
+            const patientData = res.data;
+            this.setState({patientData})
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Patients</h2>
+                <table align='center'>
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.patientData.map(patient=><RowCreator item={patient} />)}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export class RowCreator extends Component {
     render() {
         return (
             <div>
@@ -11,5 +45,6 @@ export class Home extends Component {
         )
     }
 }
+
 
 export default Home
